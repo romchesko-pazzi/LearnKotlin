d776c2460cf26b6e95c62e6cbf44d4d839568fb6
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2021-07-15 17:21:55 +0300

    fix : change to Double

--- src/src/main/kotlin/main.kt
+++ src/src/main/kotlin/main.kt
@@ -1,10 +1,10 @@
 fun main(args: Array<String>) {
 
     print("Введите первое значение: ")
-    var firstNumber = readLine()!!.toInt()
+    var firstNumber = readLine()!!.toDouble()
 
     print("Введите второе значение: ")
-    var secondNumber = readLine()!!.toInt()
+    var secondNumber = readLine()!!.toDouble()
 
     print("Введите операцию: ")
     var operation = readLine()
